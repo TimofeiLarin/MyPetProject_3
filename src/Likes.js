67af8bed53a657f5ab767e92d5cbf3bbf577ab26
Timofei Larin <timofeilarine31@gmail.ru>
2022-02-28 18:20:09 +0300

    Create project

---
+++ src/Likes.js
@@ -0,0 +1,21 @@
+import {connect} from 'react-redux';
+
+function Likes(props) {
+  console.log(props)
+  return (
+    <div className="button-controls">
+      <button>Like: {props.likes}</button>
+      <button>Dislike</button>
+    </div>
+  )
+}
+
+function mapStateToProps(state){
+  console.log('mapStateToProps', state);
+  return {
+    likes: state.likes
+  }
+}
+
+export default Likes;
+export default connect(mapStateToProps)(Likes);
